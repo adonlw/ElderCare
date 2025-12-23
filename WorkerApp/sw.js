f64164beb839672063f671d9df7476ac0aaed3a0
adonlw <hs@nelsonanglican.nz>
2025-12-24 11:50:18 +1300

    Delete WorkerApp directory

--- WorkerApp/sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE_NAME='v78.32_1766528598686';const u=['./','./index.html','./manifest.json'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(u)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>{return r||fetch(e.request)}))});
